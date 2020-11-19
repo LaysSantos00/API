558860b5c6946464167518ca198c6cc7dc52c942
LaysSantos00 <laysdias00@gmail.com>
2020-11-18 23:03:59 -0300

    alteração no controller

--- src/main/java/com/api/Controller/TesteController.java
+++ src/main/java/com/api/Controller/TesteController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TesteController {
-	@GetMapping("index")
+	@GetMapping("/index")
 	public String index() {
 		return "index";
 	}
