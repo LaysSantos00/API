4d51fb9ff6df5675054e0d3e312238394c363207
LaysSantos00 <laysdias00@gmail.com>
2020-11-18 21:55:40 -0300

    add index e .properties

--- src/main/java/com/api/Controller/TesteController.java
+++ src/main/java/com/api/Controller/TesteController.java
@@ -1,5 +1,12 @@
 package com.api.Controller;
 
-public class TesteController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class TesteController {
+	@GetMapping("index")
+	public String index() {
+		return "index";
+	}
 }
